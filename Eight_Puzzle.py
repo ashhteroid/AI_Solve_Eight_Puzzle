561e3e806e6a0ccd8896f93dab578e8d7d18e9b9
Flyinghappypuppy <ashwingopiram@gmail.com>
2015-11-05 12:50:21 -0800

    Code V 0.3

--- Eight_Puzzle.py
+++ Eight_Puzzle.py
@@ -1,9 +1,10 @@
-/*  =============================================================================
+'''  ==========================================================================
   @version  1.0
   @author   Ashwin Ramadevanahalli
   @title    Solving Eight Puzzle using A Star and Uniform cost Search.
-  ========================================================================== */
-
+ 	 =========================================================================='''
+'''
+Example
 goal_state=[[1,2,3]
 			[4,5,6]
 			[7,8,0]]
@@ -11,23 +12,6 @@ goal_state=[[1,2,3]
 init_state=[[1,8,2]
 			[0,4,3]
 			[7,6,5]]
-
-
-
-
-
-
-
-
-def reception:
-	print "Eight Puzzle Type Puzzle Solver"
-	size=int(raw_input("What Puzzle to solve(Example: '8' for Eight puzzle or '15' for fifteen puzzle"))
-	print "Please enter your puzzle as a sequence of rows. Use a zero to represent the blank"
-	input_list=[]    
-	for i in xrange(size):
-        input_list.append([int(x) for x in raw_input().split(',')])
-
-    
 
 def algo:
 	queue=init_state
@@ -39,7 +23,19 @@ def algo:
 		else 
 			queue.append(childrn of minnode)
 	print "Failure: No Solution"	
+'''
+import math 
 
 if __name__=="__main__":
-	algo(init_state,strategy)
-
+	#algo(init_state,strategy)
+	print "Eight Puzzle Type Puzzle Solver"
+	size=int(math.sqrt(int(raw_input("What Puzzle to solve(Example: '8' for Eight puzzle or '15' for fifteen puzzle) \n"))+1))
+	print "Please enter your puzzle as a sequence of comma seperated rows(Example: 1,2,3). Use a zero to represent the blank"
+	init_state=[]    
+	for i in xrange(size):
+		init_state.append([int(x) for x in raw_input().split(',')])	
+	print "Please enter desired goal state as a sequence of comma seperated rows(Example: 1,2,3). Use a zero to represent the blank"
+	goal_state=[]
+	for i in xrange(size):
+		goal_state.append([int(x) for x in raw_input().split(',')])	
+	
