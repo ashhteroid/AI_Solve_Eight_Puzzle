53170a9bea664d7afadc3f70d571831eeedfb4b3
Flyinghappypuppy <ashwingopiram@gmail.com>
2015-11-05 12:28:48 -0800

    Code V 0.1

--- Eight_Puzzle.py
+++ Eight_Puzzle.py
@@ -15,8 +15,22 @@ init_state=[[1,8,2]
 
 
 
+
+
+
+
+def reception:
+	print "Eight Puzzle Type Puzzle Solver"
+	size=int(raw_input("What Puzzle to solve(Example: '8' for Eight puzzle or '15' for fifteen puzzle"))
+	print "Please enter your puzzle as a sequence of rows. Use a zero to represent the blank"
+	input_list=[]    
+	for i in xrange(size):
+        input_list.append([int(x) for x in raw_input().split(',')])
+
+    
+
 def algo:
-	queue=init_statea
+	queue=init_state
 	while(queue not empty)
 		minnode=queue.pop(min(queue))
 		if minnode==goal_state
